4d7e25ae3598422ffe5a3bdfdf36e730643db1fc
Mdalmi <dalminas123@gmail.com>
2024-08-11 20:52:16 -0300

    commit hoje

--- empresarios/views.py
+++ empresarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Empresas
+from .models import Empresas, Documento, Metricas
+from investidores.models import PropostaInvestimento
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -59,4 +60,74 @@ def cadastrar_empresa(request):
         return redirect('/empresarios/cadastrar_empresa')
 
        
+def listar_empresas(request):
+ if not request.user.is_authenticated:
+        return redirect('/usuarios/logar')
+ 
+ if request.method == "GET":
+    empresas = Empresas.objects.filter(user=request.user)
+ return render(request, 'listar_empresas.html', {'empresas': empresas})
 
+
+def empresa(request, id):
+    
+ empresa = Empresas.objects.get(id=id)
+ if empresa.user != request.user:
+     messages.add_message(request, constants.ERROR, "Essa empresa não é sua!")
+     return redirect (f'/empresarios/listar_empresas')
+ 
+ if request.method == "GET":
+     documentos = Documento.objects.filter(empresa=empresa)
+     proposta_investimentos = PropostaInvestimento.objects.filter(empresa=empresa)
+     proposta_investimentos_enviada = proposta_investimentos.filter(status='PE')
+     return render(request, 'empresa.html', {'empresa': empresa, 'documentos': documentos, 'proposta_investimentos_enviada': proposta_investimentos_enviada})
+ 
+ 
+ 
+
+def add_doc(request, id):
+    empresa = Empresas.objects.get(id=id)
+    titulo = request.POST.get('titulo')
+    arquivo = request.FILES.get('arquivo')
+    extensao = arquivo.name.split('.')
+
+    if extensao[1] != 'pdf':
+        messages.add_message(request, constants.ERROR, "Envie apenas PDF's")
+        return redirect(f'/empresarios/empresa/{empresa.id}')
+
+    if not arquivo:
+        messages.add_message(request, constants.ERROR, "Envie um arquivo")
+        return redirect(f'/empresarios/empresa/{empresa.id}')
+
+    documento = Documento(
+    empresa=empresa,
+    titulo=titulo,
+    arquivo=arquivo
+    )
+    documento.save()
+    messages.add_message(request, constants.SUCCESS, "Arquivo cadastrado com sucesso")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
+
+def excluir_dc(request, id):
+ if documento.empresa.user != request.user:
+    messages.add_message(request, constants.ERROR, "Esse documento não é seu")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
+
+ documento = Documento.objects.get(id=id)
+ documento.delete()
+ messages.add_message(request, constants.SUCCESS, "Documento excluído com sucesso")
+ return redirect(f'/empresarios/empresa/{documento.empresa.id}')
+
+def add_metrica(request, id):
+ empresa = Empresas.objects.get(id=id)
+ titulo = request.POST.get('titulo')
+ valor = request.POST.get('valor')
+
+ metrica = Metricas(
+ empresa=empresa,
+ titulo=titulo,
+ valor=valor
+ )
+ metrica.save()
+ messages.add_message(request, constants.SUCCESS, "Métrica cadastrada com sucesso")
+ return redirect(f'/empresarios/empresa/{empresa.id}')
